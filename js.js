d6285d8afcb09c480d0fced83f8aa66ba4812fb2
Adnan Ahmed <adnan.ahmed@ultra-as.net>
2017-07-20 13:45:13 +0100

    Noughts and Crosses

--- js.js
+++ js.js
@@ -1,17 +1,61 @@
-/*function drag(ev){
-    ev.dataTransfer.setData("text", ev.target.id)
+var noughtOrCross = {
+    nought: "nought",
+    cross: "cross"
+};
+
+var rows = ["", "", ""]; var x;
+var columns = ["", "", ""]; var y;
+
+var grid = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false]
+] 
+
+var occupied = true;
+var state;
+var isGame = false;
+
+function onClick(obj) {
+    coordinates(obj);
+    checkOccupied();
+    if (isGame == true && this.occupied == false) {
+        if (this.state == noughtOrCross.nought) {
+            obj.innerHTML = "<img src='nought.png' height='200px' width='200px'/>";
+            this.state = noughtOrCross.cross;
+
+            rows[x - 1] = "nought"; columns[y - 1] = "nought";
+        }
+        else {
+            obj.innerHTML = "<img src='cross.png' height ='200px' width='200px'/>";
+            this.state = noughtOrCross.nought;
+
+            rows[x - 1] = "cross"; columns[y - 1] = "cross";
+        }
+    }
+    occupied = true;
 }
 
-function allowDrop(ev){
-    ev.preventDefault();
+function startGame(obj) {
+    obj.visibility = 'hidden';
+    var stateNum = Math.random();
+    if (stateNum > 0.5) {
+        this.state = noughtOrCross.nought;
+    }
+    else {
+        this.state = noughtOrCross.cross;
+    }
+    isGame = true;
 }
 
-function drop(ev){
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}*/
+function coordinates(obj) {
+    objID = obj.id.split("_").pop();
+    y = objID / 3; y = Math.ceil(y);
+    x = objID - (y * 3); x += 3;
+}
 
-function onClick(obj){
-    obj.innerHTML = "<img src='nought.png' height='200px' width='200px'/>"
+function checkOccupied() {
+    if(rows[this.x] == "" && columns[this.y] == ""){
+        this.occupied = false;
+    }
 }
