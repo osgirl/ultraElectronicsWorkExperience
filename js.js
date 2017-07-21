e7fd4d9ad1f16d64a1c991a12030bd79dfec931f
Adnan Ahmed <adnan.ahmed@ultra-as.net>
2017-07-21 15:04:54 +0100

    TRON

--- js.js
+++ js.js
@@ -7,9 +7,9 @@ var x;
 var y;
 
 var grid = [
-    [false, false, false],
-    [false, false, false],
-    [false, false, false]
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
 ]
 
 var winner;
@@ -22,7 +22,7 @@ var gameOver = false;
 function onClick(obj) {
     coordinates(obj);
     checkOccupied();
-    if (isGame == true && this.occupied == false && gameOver == false) {
+    if (isGame == true && occupied == false && gameOver == false) {
         if (this.state == noughtOrCross.nought) {
 
             obj.innerHTML = "<img src='nought.png' height='200px' width='200px'/>";
@@ -36,7 +36,14 @@ function onClick(obj) {
 
             grid[y - 1][x - 1] = "x";
         }
+        var winDown = checkDown();
+        var winAcross = checkAcross();
+        var winDiagonal = checkDiagonal();
         gameCount();
+    }
+    if (winDown == true || winAcross == true || winDiagonal == true) {
+        alert("The winner is " + winner + "!");
+        resetBoard();
     }
 }
 
@@ -67,16 +74,91 @@ function checkOccupied() {
     }
 }
 
-function checkDownLeft() {
-    for(i = 0; i < 3; i ++){
-             
+function checkDown() {
+    for (i = 0; i < grid.length; i++) {
+        if (grid[i][x - 1] != grid[0][x - 1]) {
+            return false;
+            break;
+        };
+    }
+    if (grid[0][x - 1] == "x") {
+        winner = "Crosses";
+    }
+    else {
+        winner = "noughts";
+    }
+    return true;
+}
+
+function checkAcross() {
+    for (i = 0; i < grid.length; i++) {
+        if (grid[y - 1][i] != grid[y - 1][0]) {
+            return false;
+            break;
+        }
+    }
+    if (grid[y - 1][0] == "x") {
+        winner = "Crosses";
+    }
+    else {
+        winner = "noughts";
+    }
+    return true;
+}
+
+function checkDiagonal() {
+    if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] == grid[2][2] && grid[1][1] != "") {
+        if (grid[1][1] == "x") {
+            winner = "crosses";
+        }
+        else {
+            this.winner = "noughts";
+        }
+        return true;
+    }
+    else if (grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2] && grid[2][0] == grid[0][2] && grid[1][1] != "") {
+        if (grid[1][1] == "x") {
+            winner = "crosses";
+        }
+        else {
+            winner = "noughts";
+        }
+        return true;
+    }
+    else {
+        return false;
     }
 }
 
 function gameCount() {
     gamecount++;
-    if (gamecount >= 9) {
+    if (gamecount >= 9 && winner == null) {
         gameOver = true;
-        alert("Ran out of space. Stalemate!");
+        on();
+        document.getElementById("image").innerHTML = "<img align='middle' src='http://cdn5.thr.com/sites/default/files/2011/06/nicolas_cage_2011_a_p.jpg'/>";
+        resetBoard();
     }
+}
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function resetBoard() {
+    for (i = 1; i < 10; i++) {
+        document.getElementById("div_" + i).innerHTML = "";
+    }
+    grid = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+    gamecount = 0;
+    gameOver = false;
+    winner = null;
 }
